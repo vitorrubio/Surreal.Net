e3cc42474cdad08ba152f0358bdf8508348aae11
Vitor Luiz Rubio <vitorrubio@gmail.com>
2023-05-04 17:49:43 -0300

    initial commit

---
+++ Surreal.Net.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Surreal.Net.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ Surreal.Net/Surreal.cs
@@ -0,0 +1,30 @@
+﻿namespace Surreal.Net;
+public class Surreal
+{
+    private readonly string _url = "";
+
+    public Surreal(string url)
+    {
+        _url = url;
+    }
+
+    public void SignIn(string user, string pass)
+    {
+
+    }
+
+    public void Use(string ns, string db)
+    {
+
+    }
+
+    public T Create<T>(T obj) where T: class, new()
+    {
+        return obj;
+    }
+
+    public T Change<T>(object obj) where T: class, new()
+    {
+        return new T();
+    }
+}
